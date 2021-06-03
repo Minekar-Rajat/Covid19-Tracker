f705e9d93e6931b4aa687bfd33c91d16de6dfc4b
Minekar-Rajat <minekarrajat@gmail.com>
2021-06-03 11:14:48 +0530

    Deployed

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -13,19 +13,19 @@ const Footer = () => {
                         <a href="https://api.whatsapp.com/send?phone=919013353535&text=Hi&source=&data=&app_absent=" target='_blank' rel='noreferrer' title='Whatsapp'><i className='fa fa-whatsapp p-2' id='icon'></i></a>
                     </div>
                     <div className='col-4 col-sm-4 col-md-2'>
-                        <a href="https://www.facebook.com/MyGovIndia/" target='_blank' rel='noreferrer' title='Facebook'><i className='fab fa-facebook p-2' id="icon"></i></a>
+                        <a href="https://www.facebook.com/MyGovIndia/" target='_blank' rel='noreferrer' title='Facebook'><i className='fa fa-facebook-square p-2' id="icon"></i></a>
                     </div>
                     <div className='col-4 col-sm-4 col-md-2'>
-                        <a href="https://www.messenger.com/t/MyGovIndia" target='_blank' rel='noreferrer' title='Messenger'><i className='fab fa-facebook-messenger p-2' id="icon"></i></a>
+                        <a href="https://www.messenger.com/t/MyGovIndia" target='_blank' rel='noreferrer' title='Messenger'><img src='./images/messenger.png' alt='messenger' className='messenger p-0' /></a>
                     </div>
                     <div className='col-4 col-sm-4 col-md-2'>
-                        <a href="https://www.instagram.com/mygovindia/" target='_blank' rel='noreferrer' title='Instagram'><i className='fab fa-instagram p-2' id='icon'></i></a>
+                        <a href="https://www.instagram.com/mygovindia/" target='_blank' rel='noreferrer' title='Instagram'><i className='fa fa-instagram p-2' id='icon'></i></a>
                     </div>
                     <div className='col-4 col-sm-4 col-md-2'>
-                        <a href="https://t.me/MyGovCoronaNewsdesk" target='_blank' rel='noreferrer' title='Telegram'><i className="fab fa-telegram p-2" id='icon'></i></a>
+                        <a href="https://t.me/MyGovCoronaNewsdesk" target='_blank' rel='noreferrer' title='Telegram'><i className="fa fa-telegram p-2" id='icon'></i></a>
                     </div>
                     <div className='col-4 col-sm-4 col-md-2'>
-                        <a href="https://twitter.com/mygovindia" target='_blank' rel='noreferrer' title='Twitter'><i className='fab fa-twitter p-2 rounded-circle' id='icon'></i></a>
+                        <a href="https://twitter.com/mygovindia" target='_blank' rel='noreferrer' title='Twitter'><i className='fa fa-twitter p-2 rounded-circle' id='icon'></i></a>
                     </div>
                 </div>
                 <br /><br />
